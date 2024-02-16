9dec5942597b1086241c2070d0f32633bb5b4e22
Topin1SMR <apancorb@g.educaand.es>
2024-02-16 13:54:26 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Adrian y Don Jose Manuel</code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://classroom.google.com/c/NTg5ODIyNjQ0ODYz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Asignatura de APLOF
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
